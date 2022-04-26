cae3a6fa86b3e0d143a3ca1ba7bbe7e9d2068040
krB88 <raftopoulos88@gmail.com>
2022-04-26 16:58:55 +0300

    Update User credentials, login, signup and add frontend

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const path = require('path');
 const userRouter = require('./routers/user');
 const adminRouter = require('./routers/adminRouter');
 const session = require('express-session');
+const flash = require('connect-flash');
 require('./db/mongoose')
 
+app.use(express.static(__dirname+'/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //parse the request body.
 app.use(express.urlencoded({extended : true}));
 app.use(session({secret : 'notagoodsecret'}));
+app.use(flash());
 
 app.use(express.json())
 app.use(userRouter);
--- routers/user.js
+++ routers/user.js
@@ -11,9 +11,12 @@ const requiredLogin = (req, res, next)=>{
     } 
     next();
 }
+router.get('/', (req, res)=>{
+    res.redirect('/userLogin');
+})
 
-router.get('/', (req,res)=>{
-    res.send("This is the home page!");
+router.get('/homepage',requiredLogin, (req,res)=>{
+    res.render('homepage.ejs');
 })
 
 //USER SIGN UP
@@ -35,12 +38,12 @@ router.post('/userSignUp', async (req, res)=>{
     await user.save();
     //if we do succesfully login, we store the user ID in the session.
     req.session.user_id = user._id;
-    res.status(201).redirect('/');
+    res.status(201).redirect('/homepage');
 })
 
 //USER LOGIN
 router.get('/userLogin', (req,res)=>{
-    res.render('userLogin.ejs');
+    res.render('userLogin.ejs' , {messages: req.flash('failedLogin')});
 })
 
 router.post('/userLogin', async(req,res)=>{
@@ -49,21 +52,51 @@ router.post('/userLogin', async(req,res)=>{
     if(foundUser){
         //if we do succesfully login, we store the user ID in the session.
         req.session.user_id = foundUser._id;
-        res.redirect('/secret');
+        res.redirect('/homepage');
     }else{
+        req.flash('failedLogin', "Username or Password is incorrect! Try again")
         res.redirect('/userLogin');
     }
 })
 
 //USER LOGOUT
-router.post('/userLogout', (req, res)=>{
+router.get('/userLogout', requiredLogin, (req, res)=>{
     req.session.destroy();
     //when logout we redirect in the login page!
     res.redirect('/userLogin');
 })
 
-router.get('/secret', requiredLogin, (req, res)=>{
-    res.render('secret.ejs');
+//UPDATE USER
+router.get('/profile', requiredLogin, async(req, res)=>{
+    user_id = req.session.user_id;
+    const foundUser = await User.findById(user_id);
+    const username = foundUser.username;
+    const email = foundUser.email;
+    res.render('profile.ejs', {username: username, email : email});
+})
+
+router.get('/editProfile', requiredLogin, async(req, res)=>{
+    user_id = req.session.user_id;
+    const foundUser = await User.findById(user_id);
+    const email = foundUser.email;
+    res.render('editProfile.ejs', {email : email});
+})
+
+router.post('/editProfile', requiredLogin, async(req, res)=>{
+    user_id = req.session.user_id;
+    const {username, password} = req.body;
+    const isPasswordValid = await User.passwordValidation(password);
+    
+    if(isPasswordValid != null){
+        console.log("We 've got an error")
+        return res.status(400).send(isPasswordValid);
+    }
+
+    const filter = {_id : user_id};
+    hashed_password = await bcrypt.hash(password, 8);
+    const update = {username : username, password: hashed_password}
+    await User.findOneAndUpdate(filter, update);
+    res.redirect('/profile');
 })
 
 
@@ -76,7 +109,7 @@ router.get('/dataAdministration', (req,res)=>{
     res.render('dataAdministration.ejs')
 })
 
-router.get('/statistics', (req, res)=>{
+router.get('/statistics',requiredLogin, (req, res)=>{
     res.render('statistics.ejs')
 })
 
